aabd8b5752c0f03793ad9df0457e249aaec7aa5a
Dominik Boehler <dominik.boehler@gmx.net>
2022-11-08 10:58:46 +0100

    fixed scrolling issues for long files

--- myfunctions.fish
+++ myfunctions.fish
@@ -66,7 +66,7 @@ function vimGoToFiles
 end;
 
 function displayRgPipedFzf
-    echo (rg . -n --glob "!.git/" --glob "!vendor/" --glob "!node_modules/" | fzf -d ':' -n 2.. --ansi --no-sort --preview 'bat --style=numbers --color=always --highlight-line {2} {1}');
+  echo $(rg . -n --glob '!.git/' --glob '!vendor/' --glob '!node_modules/' | fzf -d ':' -n 2.. --ansi --no-sort --preview 'bat --style=numbers --color=always --highlight-line {2} {1};' --preview-window +{2}-5)
 end;
 
 function nvimGoToLine
