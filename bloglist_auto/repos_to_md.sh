79c3a56a11236630d49ad61ca6e44045b1257282
Dominik Boehler <dominik.boehler@gmx.net>
2023-11-21 13:06:25 +0100

    rearranged repo

---
+++ bloglist_auto/repos_to_md.sh
@@ -0,0 +1,1 @@
+echo "| Name | Homepage URL | Description |" >  bloglist_auto.md | echo "|---|---|---|" >> bloglist_auto.md | gh repo list 23W-GBAC -L 100 --json name --json description --json stargazerCount --json homepageUrl | jq -r '.[] | [.name, .homepageUrl, .description] | @tsv' | sed 's/\t/ | /g' | sed 's/^/| /g' | sed 's/$/ |/g' >> bloglist_auto.md
