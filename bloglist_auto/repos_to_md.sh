6ddd2a989e4aec025f64432b81fab22a8db4e57c
Dominik Böhler <dominik.boehler@gmx.net>
2023-11-28 10:55:57 +0100

    Update repos_to_md.sh

--- bloglist_auto/repos_to_md.sh
+++ bloglist_auto/repos_to_md.sh
@@ -1,10 +1,12 @@
-header = "| Name | Homepage URL | Description |"  
-separator = "|---|---|---|"
+#!/bin/bash
 
-json = gh repo list 23W-GBAC -L 100 --json name --json description --json stargazerCount --json homepageUrl
-body = $json | jq -r '.[] | [.name, .homepageUrl, .description] | @tsv' | sed 's/\t/ | /g'
-body = $body | sed 's/^/| /g'
-body = $body | sed 's/$/ |/g'
+HEADER = "| Name | Homepage URL | Description |"  
+SEPARATOR = "|---|---|---|"
+
+JSON = gh repo list 23W-GBAC -L 100 --json name --json description --json stargazerCount --json homepageUrl
+BODY = $JSON | jq -r '.[] | [.name, .homepageUrl, .description] | @tsv' | sed 's/\t/ | /g'
+BODY = $BODY | sed 's/^/| /g'
+BODY = $BODY | sed 's/$/ |/g'
 
 # final output of all segments of our code, feeds into a new file called bloglist_auto.md
-echo $header && echo $separator && echo $body > bloglist_auto.md
+echo $HEADER && echo $SEPARATOR && echo $BODY > bloglist_auto.md
