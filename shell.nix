ff420e743bf29f94ad0d884dea0e81cc72574e95
Dominik Boehler <dominik.boehler@gmx.net>
2023-12-05 11:50:20 +0100

    added code for member visualization

---
+++ shell.nix
@@ -0,0 +1,23 @@
+
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  pythonPackages = with pkgs.python311Packages; [
+    fastapi
+    uvicorn
+    requests
+    python-dotenv
+    pandas
+    jupyterlab
+    ipython
+    pip
+  ];
+# https://ryantm.github.io/nixpkgs/languages-frameworks/python/#how-to-consume-python-modules-using-pip-in-a-virtual-environment-like-i-am-used-to-on-other-operating-systems
+in pkgs.mkShell rec {
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pkgs.python311
+    pythonPackages
+  ];
+}
